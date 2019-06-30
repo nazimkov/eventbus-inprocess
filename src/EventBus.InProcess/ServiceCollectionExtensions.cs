6b01f738b9a22c80f8580c9ddc7cb03856e1151d
nazimkov <vyacheslav.nazimkov@gmail.com>
2019-06-30 14:59:19 +0300

    add service colletion extensions for simple registration

---
+++ src/EventBus.InProcess/ServiceCollectionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using EventBus.InProcess.Internals;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace EventBus.InProcess
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddEventBus(this IServiceCollection services)
+        {
+            services.AddSingleton<IEventBusSubscriptionManager, InMemorySubscriptionManager>();
+            services.AddSingleton<IChannelManager, ChannelManager>();
+            services.AddSingleton<IEventBus, EventBusInProcess>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddSubscription<T, TH>(
+            this IServiceCollection services)
+            where TH : class, IIntegrationEventHandler<T>
+            where T : IntegrationEvent
+        {
+            services.AddScoped<TH>();
+
+            var bus = services
+                .BuildServiceProvider() // TODO Rethink building service provider each time
+                .GetRequiredService<IEventBus>();
+
+            bus.Subscribe<T, TH>();
+
+            return services;
+        }
+    }
+}
