1220b76b26b278421ea22f60e96ea10d51edd2f9
nazimkov <vyacheslav.nazimkov@gmail.com>
2019-07-13 13:34:12 +0300

    draft service collection implementation

---
+++ src/EventBus.InProcess/IHandlerFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace EventBus.InProcess
+{
+    public interface IServiceFactory
+    {
+        THandler GetInstance<THandler>();
+
+        object GetInstance(Type type);
+    }
+
+    internal class DafaultServiceFactory : IServiceFactory
+    {
+        private readonly ConcurrentDictionary<Type, object> _handlers = new ConcurrentDictionary<Type, object>();
+
+        public THandler GetInstance<THandler>() => (THandler)GetInstance(typeof(THandler));
+
+        public object GetInstance(Type type)
+        {
+            var handler = _handlers.GetOrAdd(type, t => Activator.CreateInstance(type));
+            return handler;
+        }
+    }
+}
